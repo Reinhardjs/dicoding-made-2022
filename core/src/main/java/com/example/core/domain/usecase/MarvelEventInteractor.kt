ed6d5f24c9c04337ed9ac0b20749251a0a697169
reinhardjs <reinhardjsilalahi@gmail.com>
2022-10-30 02:30:13 +0700

    added domain's usecase interactor

---
+++ core/src/main/java/com/example/core/domain/usecase/MarvelEventInteractor.kt
@@ -0,0 +1,21 @@
+package com.example.core.domain.usecase
+
+import com.example.core.data.Resource
+import com.example.core.domain.model.MarvelEvent
+import com.example.core.domain.repository.IMarvelEventRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class MarvelEventInteractor @Inject constructor(private val marvelEventRepository: IMarvelEventRepository) :
+    MarvelEventUseCase {
+
+    override fun getAllMarvelEvent(): Flow<Resource<List<MarvelEvent>>> =
+        marvelEventRepository.getAllMarvelEvent()
+
+    override fun getAlLFavouriteMarvelEvent(): Flow<List<MarvelEvent>> =
+        marvelEventRepository.getAllFavouriteMarvelEvent()
+
+    override fun setFavouriteMarvelEvent(marvelEvent: MarvelEvent, state: Boolean) =
+        marvelEventRepository.setFavouriteMarvelEvent(marvelEvent, state)
+
+}
