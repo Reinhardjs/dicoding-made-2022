820f4d7d75192ef61be10316f371dc1770c4db00
reinhardjs <reinhardjsilalahi@gmail.com>
2022-10-31 20:45:46 +0700

    refactor directory structure

--- app/src/main/java/com/example/dicoding_made_2022/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/example/dicoding_made_2022/ui/activities/main/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.example.dicoding_made_2022
+package com.example.dicoding_made_2022.ui.activities.main;
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import com.example.dicoding_made_2022.R
 import com.example.dicoding_made_2022.databinding.ActivityMainBinding
 import com.example.dicoding_made_2022.home.HomeFragment
 import com.google.android.material.navigation.NavigationView
