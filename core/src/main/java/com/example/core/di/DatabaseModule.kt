2e7cf8b2e86b6cb7defa8aaf90677889ea20148e
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-04 23:40:06 +0700

    encrypt database

--- core/src/main/java/com/example/core/di/DatabaseModule.kt
+++ core/src/main/java/com/example/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,11 +19,16 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): MarvelEventDatabase = Room.databaseBuilder(
-        context,
-        MarvelEventDatabase::class.java, "MarvelEvent.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): MarvelEventDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
+            context,
+            MarvelEventDatabase::class.java, "MarvelEvent.db"
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
+    }
 
     @Provides
-    fun provideMarvelEventDao(database: MarvelEventDatabase): MarvelEventDao = database.marvelEventDao()
+    fun provideMarvelEventDao(database: MarvelEventDatabase): MarvelEventDao =
+        database.marvelEventDao()
 }
