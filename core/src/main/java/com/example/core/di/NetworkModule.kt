e48992e99ab5e828cf6b2c3e4e3ad448505febbf
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-04 23:58:28 +0700

    added ssl pinning

--- core/src/main/java/com/example/core/di/NetworkModule.kt
+++ core/src/main/java/com/example/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,10 +18,16 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val hostname = "gateway.marvel.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/gnNGtYKJyibWzC+q/eEcbLXYhhyKrrf8D54FCethNtg=")
+            .add(hostname, "sha256/980Ionqp3wkYtN9SZVgMzuWQzJta1nfxNPwTem1X0uc=")
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
